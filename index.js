128854cd60e50c616b4e03d860b78979930104f1
CloudCorp <37921410+CloudCorp@users.noreply.github.com>
2018-04-03 09:53:16 +1000

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity('https://git.io/d.js-heroku', {type: 'WATCHING'});
+    client.user.setActivity('My Console For Errors', {type: 'WATCHING'});
 });
 
 client.on('message', msg => {
