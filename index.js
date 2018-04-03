3a18f1cb13cb5ac0ede1f04ec9218b01d945e318
CloudCorp <37921410+CloudCorp@users.noreply.github.com>
2018-04-03 10:38:30 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 // Discord.js bot
 const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity('My Console For Errors', {type: 'WATCHING'});
+    client.user.setActivity('My Consol', {type: 'WATCHING'});
 });
 
-client.on('message', msg => {
-    if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
-    const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
-    const args = msg.content.split(' ').slice(1).join(' ');
-    if (command === 'guide') return msg.channel.send('https://git.io/d.js-heroku');
-    else if (command === 'invite') return msg.channel.send(process.env.INVITE);
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'ping') {
+		message.channel.send('Pong.');
+	}
+	else if (command === 'beep') {
+		message.channel.send('Boop.');
+	}
+	
 });
 
 client.login(process.env.TOKEN);
