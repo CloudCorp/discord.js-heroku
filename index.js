97964654c2deb6537450be868b6e3d9f9f8db136
CloudCorp <37921410+CloudCorp@users.noreply.github.com>
2018-04-03 10:49:00 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 
+client.login(process.env.TOKEN);
+
 const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
@@ -8,14 +10,26 @@ client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands');
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
-}
+
+
+
+
+fs.readdir("./commands/", (err, files) => {
+    if(err) console.error(err);
+
+    let cmdfiles = files.filter(f => f.split(".").pop() === "js");
+    if(cmdfiles.length <= 0) return console.log('No commands to load!');
+
+    console.log(`${cmdfiles.length} commands are being loaded.`);
+    jsfiles.forEach((f, i) => {
+        let props = require(`./commands/${f}`);
+        console.log(`Command ${i+1}: ${f} loaded!`);
+        client.commands.set(command.name, command);
+    });
+});
 
 client.on('ready', () => {
-    client.user.setActivity('My Consol', {type: 'WATCHING'});
-
+    client.user.setActivity('My Console', {type: 'WATCHING'});
 });
 
 client.on('message', message => {
@@ -24,67 +38,18 @@ client.on('message', message => {
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
 
-	if (command === 'ping') {
-		message.channel.send('Pong.');
-	}
-	else if (command === 'beep') {
-		message.channel.send('Boop.');
-	}
-	else if (command === 'server') {
-		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-	}
-	else if (command === 'user-info') {
-		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
-	}
-	else if (command === 'info') {
-		if (!args.length) {
-			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-		}
-		else if (args[0] === 'foo') {
-			return message.channel.send('bar');
-		}
+	if (!client.commands.has(command)) return;
 
-		message.channel.send(`First argument: ${args[0]}`);
+	try {
+		client.commands.get(command).execute(message, args);
 	}
-	else if (command === 'kick') {
-		if (!message.mentions.users.size) {
-			return message.reply('you need to tag a user in order to kick them!');
-		}
-
-		const taggedUser = message.mentions.users.first();
-
-		message.channel.send(`You wanted to kick: ${taggedUser.username}`);
-	}
-	else if (command === 'avatar') {
-		if (!message.mentions.users.size) {
-			return message.channel.send(`Your avatar: ${message.author.displayAvatarURL}`);
-		}
-
-		const avatarList = message.mentions.users.map(user => {
-			return `${user.username}'s avatar: ${user.displayAvatarURL}`;
-		});
-
-		message.channel.send(avatarList);
-	}
-	else if (command === 'prune') {
-		const amount = parseInt(args[0]) + 1;
-
-		if (isNaN(amount)) {
-			return message.reply('that doesn\'t seem to be a valid number.');
-		}
-		else if (amount <= 1 || amount > 100) {
-			return message.reply('you need to input a number between 1 and 99.');
-		}
-
-		message.channel.bulkDelete(amount, true).catch(err => {
-			console.error(err);
-			message.channel.send('there was an error trying to prune messages in this channel!');
-		});
+	catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
 	}
 });
 
+
 client.login(process.env.TOKEN);
-
-
 
 
